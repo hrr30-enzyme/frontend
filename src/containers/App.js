e97ceb846f9dcae66a287705a356dcafc6f20b7d
William Cory <willcory10@gmail.com>
2018-05-10 11:52:07 -0700

    fix merge conflict

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import { Switch, Route } from 'react-router-dom'
-=======
-import { Switch, Route, onEnter } from 'react-router-dom'
->>>>>>> a9c8de28be963d190d5e868e0390bbe4d4826a3a
 import Home from './Home';
 import Landing from './Landing'
 import LogIn from './LogIn'
