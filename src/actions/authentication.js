a9c8de28be963d190d5e868e0390bbe4d4826a3a
William Cory <willcory10@gmail.com>
2018-05-10 09:18:24 -0700

    format code

--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -1,59 +1,63 @@
-import axios from 'axios'
+import axios from "axios";
+import {
+  SIGN_IN_FAILED,
+  SIGN_IN_SUCCESSFUL,
+  SIGN_OUT_FAILED,
+  SIGN_OUT_SUCCESSFUL
+} from "../actions/types";
 
-function sendSignIn(credentials){
-    return axios.post('/user/sign-in', credentials)
-}
+export const sendSignIn = credentials => {
+  return axios.post("/user/sign-in", credentials);
+};
 
-export const signedIn = (userInfo) => ({
-  type: 'SIGN_IN_SUCCESSFUL',
+export const signedIn = userInfo => ({
+  type: SIGN_IN_SUCCESSFUL,
   payload: {
     userInfo
   }
-})
+});
 
-export const doNotSignIn = (error) => ({
-        type: 'SIGN_IN_FAILED',
-        payload: {
-            error
-        }
-    }
-)
+export const doNotSignIn = error => ({
+  type: SIGN_IN_FAILED,
+  payload: {
+    error
+  }
+});
 
-export function signIn (credentials){
-    return function(dispatch){
-        return sendSignIn(credentials).then(
-            userInfo => dispatch(signedIn(userInfo)),
-            error => dispatch(doNotSignIn(error))
-        );
-    };
+export const signIn = credentials => {
+  return function(dispatch) {
+    return sendSignIn(credentials).then(
+      userInfo => dispatch(signedIn(userInfo)),
+      error => dispatch(doNotSignIn(error))
+    );
+  };
+};
+
+function sendSignOut(credentials) {
+  return axios.post("/user/sign-out", credentials);
 }
 
-function sendSignOut(credentials){
-    return axios.post('/user/sign-out', credentials)
-}
-
-function signedOut (status){
-    return {
-        type: 'SIGN_OUT_SUCCESSFUL',
-        payload: {
-            status
-        }
-    }
-}
-
-const doNotSignOut = (error) => ({
-    type: 'SIGN_OUT_FAILED',
+function signedOut(status) {
+  return {
+    type: SIGN_OUT_SUCCESSFUL,
     payload: {
-        error
+      status
     }
-})
-
-
-export const signOut = function(credentials){
-    return function(dispatch){
-        return sendSignOut(credentials).then(
-            status => dispatch(signedOut(status)),
-            error => dispatch(doNotSignOut(error))
-        );
-    };
+  };
 }
+
+const doNotSignOut = error => ({
+  type: SIGN_OUT_FAILED,
+  payload: {
+    error
+  }
+});
+
+export const signOut = function(credentials) {
+  return function(dispatch) {
+    return sendSignOut(credentials).then(
+      status => dispatch(signedOut(status)),
+      error => dispatch(doNotSignOut(error))
+    );
+  };
+};
--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-
-import Usertag from './Usertag'
+import Usertag from "./Usertag";
 
 const Div = styled.div`
   display: grid;
@@ -14,20 +13,25 @@ const Div = styled.div`
     background-color: white;
     grid-column: 1/3;
   }
-`
+`;
 
-const Answer = (props) => {
-
+const Answer = props => {
   return (
     <Div>
       <div className="usertag">
         <Usertag />
       </div>
       <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum."
       </p>
     </Div>
-  )
-}
+  );
+};
 
-export default Answer
+export default Answer;
