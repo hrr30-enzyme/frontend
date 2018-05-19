577ed6529e865fbe7f521090173bfadbf322b41d
William Cory <willcory10@gmail.com>
2018-05-19 15:14:50 -0700

    use localhost to check to see if in production

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import promise from 'redux-promise-middleware'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { createLogger } from 'redux-logger'
 
+import { ORIGIN, LOCALHOST } from '../constants'
 import rootReducer from '../reducers'
 
 const initialState = {};
@@ -13,10 +14,10 @@ const getMiddleware = () => {
   const middleware = [promise(), thunk];
   const devMiddleware = [...middleware, createLogger()];
 
-  if (process.env.NODE_ENV === 'production') {
-    return applyMiddleware(...middleware)
-  } else {
+  if (ORIGIN === LOCALHOST) {
     return composeWithDevTools(applyMiddleware(...devMiddleware));
+  } else {
+    return applyMiddleware(...middleware)
   }
 };
 
