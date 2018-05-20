7b9ec2cbeed74dc5b7d5704d80e3f7d34c9082a9
Kyle Shifflett <kshifflett707@gmail.com>
2018-05-20 01:24:16 -0700

    Styled QuestionsPage layout

--- src/components/QuestionPreview.js
+++ src/components/QuestionPreview.js
@@ -5,53 +5,136 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Question = styled.div`
-  display: block
-  padding: 12px;
-  border-bottom: 1px solid #e4e6e8;
-  width: 728px;
+  display: grid;
+  grid-template-rows: auto auto;
+  grid-template-columns: 20% 10% auto auto;
+  grid-column-gap: 35px;
+  grid-row-gap: 1em;
+  text-decoration: none;
+  width: auto;
+  height: auto;
+  margin: 2px 2px;
 `;
 
 const Stats = styled.div`
-  float: left;
-  vertical-align: top;
-  margin-right: 50px;
-  cursor: pointer;
+  display: grid;
+  grid-template-columns: 40px 50px 50px;
+  grid-row: 1 / span 2;
+  grid-column: 1;
+  grid-column-gap: 5px;
+  justify-self: left;
+  margin-bottom: 10px;
 `;
+
 const Votes = styled.div`
   display: inline-block;
-  height: 38px;
-  min-width: 38px;
-  margin: 0 3px 0 0;
-  font-size: 11px;
+  grid-column: 1;
+  font-size: 14px;
 `;
 
 const Answers = styled.div`
   display: inline-block;
-  margin: 0 3px 0 0;
-  min-width: 44px;
-  height: auto;
-  font-size: 11px;
+  grid-column: 2;
+  font-size: 14px;
 `;
 
 const Views = styled.div`
   display: inline-block;
-  height: 38px;
-  min-width: 40px;
-  margin: 0 7px 0 0;
-  font-size: 11px;
+  grid-column: 3;
+  font-size: 14px;
 `;
 
 const Inner = styled.div`
-  font-size: 17px;
+  font-size: 22px;
   font-weight: 300;
   color: #6a737c;
-  margin-bottom: 2px
-  display: block
+  margin-top: 10px;
+  display: block;
+  text-align: center;
 `;
+
 const MiniCount = styled.div`
   padding: 8px 5px;
   line-height: 1;
 `; 
+
+const Bounty = styled.div`
+  grid-row: 1;
+  grid-column: 2;
+  justify-self: left;
+  align-self: center;
+  font-weight: bolder;
+  font-size: 18px;
+  font-style: italic;
+`
+
+const Reward = styled.div`
+  grid-row: 2;
+  grid-column: 2;
+  border: solid 2px #A50104;
+  margin-bottom: 10px;
+  justify-self: left;  
+`
+
+const Title = styled.div`
+  grid-row: 1;
+  grid-column: 3;
+  font-size: 24px;
+  text-decoration: none;
+`
+
+const User = styled.div`
+  grid-row: 1;
+  grid-column: 4;
+  font-size: 18px;
+  text-decoration: none;
+  justify-self: left;
+`
+
+const Tags = styled.div`
+  grid-row: 2;
+  grid-column: 3;
+  display: grid;
+  grid-template-columns: auto;
+  grid-column-gap: 5px;
+  margin-bottom: 10px;
+`
+
+const Tag1 = styled.div`
+  grid-column: 1;
+  border: solid 2px yellow;
+  border-radius: 4px;
+`
+
+const Tag2 = styled.div`
+  grid-column: 2;
+  border: solid 2px #FCBA04;
+  border-radius: 4px;  
+`
+
+const Tag3 = styled.div`
+  grid-column: 3;
+  border: solid 2px #A50104;
+  border-radius: 4px;  
+`
+
+const Tag4 = styled.div`
+  grid-column: 4;
+  border: solid 2px #590004;
+  border-radius: 4px;  
+`
+
+const Tag5 = styled.div`
+  grid-column: 5;
+  border: solid 2px #250001;
+  border-radius: 4px;  
+`
+
+const Tag6 = styled.div`
+  grid-column: 6;
+  border: solid 2px #000000;
+  border-radius: 4px;  
+`
 
 const handleClick = (e, callback, cb, id) => {
   e.preventDefault();
@@ -64,7 +147,7 @@ const handleClick = (e, callback, cb, id) => {
 const QuestionPreview = (props) => {
   console.log('QuestionPreview: ' + props);
   return (
-    <Link to={`/question/${props.id}`} >
+    <Link to={`/question/${props.id}`} style={{textDecoration: 'none'}}>
       <Question >
       <Stats>
         <MiniCount>
@@ -72,14 +155,14 @@ const QuestionPreview = (props) => {
             <Inner>
               <div display="block">{props.upvoteCount}</div>
             </Inner>{" "}
-            vote
+            votes
           </Votes>
         </MiniCount>
         <MiniCount>
           <Answers>
             <Inner>
-              <div display="block">{props.answerCount}</div>
-            </Inner>{" "}
+              {props.answerCount}
+            </Inner>
             answers
           </Answers>
         </MiniCount>
@@ -92,9 +175,26 @@ const QuestionPreview = (props) => {
           </Views>
         </MiniCount>
       </Stats>
-      Title: {props.title}
-      <br/>
-      Body: {props.body}
+      <Bounty>
+        Bounty
+      </Bounty>
+      <Reward>
+        100 ETH
+      </Reward>
+      <Title>
+        {props.title}
+      </Title>
+      <User>
+        User
+      </User>
+      <Tags>
+        <Tag1>TAG1</Tag1>
+        <Tag2>TAG2</Tag2>
+        <Tag3>TAG3</Tag3>
+        <Tag4>TAG4</Tag4> 
+        <Tag5>TAG5</Tag5>
+        <Tag6>TAG6</Tag6>       
+      </Tags>
     </Question>
     </Link>
   );
